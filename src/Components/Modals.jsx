945418f1d9b11e0a2ccc5c914b6e6df639e267f6
Yusuf <refsilva@yahoo.com>
2024-05-04 16:39:42 +0100

    added new useref for the login

--- src/Components/Modals.jsx
+++ src/Components/Modals.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect } from "react";
-import { ShopContext } from "../Context/ShopContext";
-import { useContext } from "react";
-import { FaTimes } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { ShopContext } from '../Context/ShopContext';
+import { useContext } from 'react';
+import { FaTimes } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 const Modals = ({ Products, setisOpen, isOpen }) => {
   const Cart = useContext(ShopContext);
   let cc = Cart.totalCart();
@@ -20,49 +20,49 @@ const Modals = ({ Products, setisOpen, isOpen }) => {
 
   return (
     <>
-      <div className=" modal top-20 w-4/5 md:w-4/5 lg:w-1/4 me-3  z-20 text-black fixed right-2 ">
-        <div className="relative  h-[400px]">
-          <div className="flex top-0 z-50 bg-white justify-between p-3 border-b-8 border-teal-200 w-full md:w-2/5 md:ms-auto lg:w-full absolute left-0 right-0  ms-auto">
-            {" "}
-            <p className="font-bold">Carts</p>
+      <div className=' modal top-20 w-4/5 md:w-4/5 lg:w-1/4 me-3  z-20 text-black fixed right-2 '>
+        <div className='relative  h-[350]'>
+          <div className='flex top-0 z-50 bg-white justify-between p-3 border-b-8 border-teal-200 w-full md:w-2/5 md:ms-auto lg:w-full absolute left-0 right-0  ms-auto'>
+            {' '}
+            <p className='font-bold'>Carts</p>
             <FaTimes onClick={() => setisOpen(!isOpen)} />
           </div>
 
-          <div className=" pt-16 pb-16 bg-white h-[400px] px-5 overflow-auto w-full md:w-2/5 md:ms-auto lg:w-full">
+          <div className=' pt-16 pb-16 bg-white h-[350px] px-5 overflow-auto w-full md:w-2/5 md:ms-auto lg:w-full'>
             {Products.map((item, index) => {
               if (Cart.getProductQuantity(item.id) > 0) {
                 return (
                   <div key={index}>
                     <div
-                      className=" flex gap-2 mx-auto mb-5 w-full justify-center items-center "
+                      className=' flex gap-2 mx-auto mb-5 w-full justify-center items-center '
                       key={index}
                     >
                       <img
                         src={item.image}
-                        className="w-[70px] h-[70px]"
-                        alt="Logo"
+                        className='w-[70px] h-[70px]'
+                        alt='Logo'
                       />
                       {/* <p className="text-xs">{item.title}</p> */}
-                      <div className="flex-row">
-                        <h2 className="text-sm font-bold">
+                      <div className='flex-row'>
+                        <h2 className='text-sm font-bold'>
                           {item.title.substring(0, 20)}
                         </h2>
                         <p>{item.price}</p>
                       </div>
 
-                      <div className="flex items-center gap-4 my-4 ">
+                      <div className='flex items-center gap-4 my-4 '>
                         <button
                           onClick={() => Cart.removeCart(item.id)}
-                          className="text-teal-600 rounded-full w-9 h-9 bg-gray-200 pb-1 text-2xl"
+                          className='text-teal-600 rounded-full w-9 h-9 bg-gray-200 pb-1 text-2xl'
                         >
-                          -{" "}
+                          -{' '}
                         </button>
 
                         <p>{Cart.getProductQuantity(item.id)}</p>
 
                         <button
                           onClick={() => Cart.AddCart(item.id)}
-                          className="text-teal-600 rounded-full w-9 h-9 pb-1 bg-gray-200 text-2xl"
+                          className='text-teal-600 rounded-full w-9 h-9 pb-1 bg-gray-200 text-2xl'
                         >
                           +
                         </button>
@@ -85,16 +85,16 @@ const Modals = ({ Products, setisOpen, isOpen }) => {
               </button>
             </div>
           </div> */}
-          <div className="flex justify-between  w-full md:w-2/5 md:ms-auto lg:w-full  p-3 bg-green-300 border-t-8 border-teal-200 absolute bottom-0 left-0 right-0 ms-auto">
-            <div className="flex justify-between w-full items-center">
-              <h3 className="flex text-bold text-xl gap-3 font-bold ">
+          <div className='flex justify-between  w-full md:w-2/5 md:ms-auto lg:w-full  p-3 bg-green-300 border-t-8 border-teal-200 absolute bottom-0 left-0 right-0 ms-auto'>
+            <div className='flex justify-between w-full items-center'>
+              <h3 className='flex text-bold text-xl gap-3 font-bold '>
                 Total: <p> ₦{Math.ceil(cc)}</p>
               </h3>
-              <Link to="/checkout">
+              <Link to='/checkout'>
                 <button
                   onClick={() => setisOpen(!isOpen)}
                   disabled={!cc}
-                  className="bg-teal-700 py-2 px-3 disabled:bg-opacity-20 text-white text-sm rounded-3xl"
+                  className='bg-teal-700 py-2 px-3 disabled:bg-opacity-20 text-white text-sm rounded-3xl'
                 >
                   Checkout
                 </button>
--- src/Pages/Checkout.jsx
+++ src/Pages/Checkout.jsx
@@ -1,13 +1,13 @@
-import React, { useContext, useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { ShopContext } from "../Context/ShopContext";
-import { States } from "../Utils/Countries";
-import { IoTrashBin } from "react-icons/io5";
-import { DeliveryPrice } from "../Utils/StateDelivery";
-import { DeliveryDate } from "../Utils/DeliveryDate";
-import { doc, getDoc } from "firebase/firestore";
-import { db } from "../Config/Firebase";
-import { AuthContext } from "../Context/AuthContext";
+import React, { useContext, useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ShopContext } from '../Context/ShopContext';
+import { States } from '../Utils/Countries';
+import { IoTrashBin } from 'react-icons/io5';
+import { DeliveryPrice } from '../Utils/StateDelivery';
+import { DeliveryDate } from '../Utils/DeliveryDate';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../Config/Firebase';
+import { AuthContext } from '../Context/AuthContext';
 
 const Checkout = () => {
   const navigate = useNavigate();
@@ -16,15 +16,16 @@ const Checkout = () => {
   const [isChecekd, setisChecekd] = useState(false);
   const [states, setstates] = useState(States);
   const [FormData, setFormData] = useState({
-    fname: "",
-    lname: "",
-    Address: "",
-    City: "",
-    State: "",
-    Zip: "",
-    email: "",
-    number: "",
+    fname: '',
+    lname: '',
+    Address: '',
+    City: '',
+    State: '',
+    Zip: '',
+    email: '',
+    number: '',
   });
+  const [loading, setloading] = useState(false);
   const Cart = useContext(ShopContext);
   let TotalPrice = Cart.totalCart();
   // let removeCart = Cart.clearCart()
@@ -54,31 +55,26 @@ const Checkout = () => {
   };
 
   const retriveUserData = async () => {
-    const docRef = doc(db, "users", currentUser.uid);
+    const docRef = doc(db, 'users', currentUser.uid);
     const docSnap = await getDoc(docRef);
     let list = [];
     if (docSnap.exists()) {
-      console.log("Document data:", docSnap.data());
+      console.log('Document data:', docSnap.data());
       //   list.push(docSnap.data());
       setData([docSnap.data()]);
+      setloading(true);
     } else {
       // docSnap.data() will be undefined in this case
-      console.log("No such document!");
+      console.log('No such document!');
     }
   };
 
   useEffect(() => {
-    // if (currentUser) {
+    loading == false && alert('not ready');
+    if (!Data[0]) return 'Loading...';
     retriveUserData();
-    // } else {
-    //   navigate("/Login");
-    //   setTimeout(() => {
-    //     alert("You need to Login To Purchase Any product");
-    //   }, 1000);
-    // }
   }, []);
 
-  if (!Data[0]) return "Loading...";
   const checkHandler = (e) => {
     e.preventDefault();
     setisChecekd(!isChecekd);
@@ -98,79 +94,79 @@ const Checkout = () => {
   // }
 
   return (
-    <section className="grid grid-cols-1 lg:grid-cols-6  lg:w-4/6  w-5/6 lg:gap-9 gap-2  mx-auto pt-24">
-      <div className="col-span-4 order-last lg:order-first">
-        <h2 className="font-bold text-3xl">Who is Getting This Order</h2>
-        <hr className="w-full my-5" />
-        <h3 className=" my-5 text-xl text-gray-500">Shipping Information</h3>
+    <section className='grid grid-cols-1 lg:grid-cols-6  lg:w-4/6  w-5/6 lg:gap-9 gap-2  mx-auto pt-24'>
+      <div className='col-span-4 order-last lg:order-first'>
+        <h2 className='font-bold text-3xl'>Who is Getting This Order</h2>
+        <hr className='w-full my-5' />
+        <h3 className=' my-5 text-xl text-gray-500'>Shipping Information</h3>
 
-        <form action="" onSubmit={handleSubmit}>
-          <div className="lg:flex flex-row  gap-7">
-            <div className="lg:w-2/4 w-full ">
-              <div className="mb-4">
-                <label htmlFor="" className="block font-bold">
+        <form action='' onSubmit={handleSubmit}>
+          <div className='lg:flex flex-row  gap-7'>
+            <div className='lg:w-2/4 w-full '>
+              <div className='mb-4'>
+                <label htmlFor='' className='block font-bold'>
                   First Name
                 </label>
                 <input
-                  type="text"
-                  id="fname"
+                  type='text'
+                  id='fname'
                   onChange={FormValues}
                   value={FormData.fname}
-                  className="input-primary"
+                  className='input-primary'
                 />
               </div>
-              <div className="mb-4">
-                <label htmlFor="" className="block font-bold">
+              <div className='mb-4'>
+                <label htmlFor='' className='block font-bold'>
                   Last Name
                 </label>
                 <input
-                  type="text"
+                  type='text'
                   onChange={FormValues}
                   value={FormData.lname}
-                  id="lname"
-                  className="input-primary"
+                  id='lname'
+                  className='input-primary'
                 />
               </div>
 
-              <div className="mb-4">
-                <label htmlFor="" className="block font-bold">
+              <div className='mb-4'>
+                <label htmlFor='' className='block font-bold'>
                   Address
                 </label>
                 <input
-                  type="text"
+                  type='text'
                   onChange={FormValues}
                   value={FormData.Address}
-                  id="Address"
-                  className="input-primary"
+                  id='Address'
+                  className='input-primary'
                 />
               </div>
-              <div className="mb-4">
-                <label htmlFor="" className="block font-bold">
+              <div className='mb-4'>
+                <label htmlFor='' className='block font-bold'>
                   City
                 </label>
                 <input
-                  type="text"
-                  id="City"
+                  type='text'
+                  id='City'
                   onChange={FormValues}
                   value={FormData.City}
-                  className="input-primary"
+                  className='input-primary'
                 />
               </div>
-              <div className="flex gap-4 mb-4">
+              <div className='flex gap-4 mb-4'>
                 <div>
-                  <label htmlFor="" className="block font-bold">
+                  <label htmlFor='' className='block font-bold'>
                     State
                   </label>
                   <select
-                    name=""
-                    id="State"
+                    name=''
+                    id='State'
                     onChange={FormValues}
                     value={FormData.State}
-                    className="input-primary text-[15px] font-bold"
+                    className='input-primary text-[15px] font-bold'
                   >
                     {states.map((item, index) => {
                       return (
-                        <option id="State" key={index}>
+                        <option id='State' key={index}>
                           {item}
                         </option>
                       );
@@ -178,109 +174,109 @@ const Checkout = () => {
                   </select>
                 </div>
                 <div>
-                  <label htmlFor="" className="block font-bold">
+                  <label htmlFor='' className='block font-bold'>
                     ZIP CODE
                   </label>
                   <input
-                    type="text"
+                    type='text'
                     onChange={FormValues}
                     value={FormData.Zip}
-                    id="Zip"
-                    className="input-primary px-1"
+                    id='Zip'
+                    className='input-primary px-1'
                   />
                 </div>
               </div>
-              <div className="flex mt-4 gap-3">
-                <input type="checkbox" name="" id="" />
+              <div className='flex mt-4 gap-3'>
+                <input type='checkbox' name='' id='' />
                 <span>Save this as my billing Adress</span>
               </div>
             </div>
-            <div className="lg:w-2/4 w-full  lg:my-0 my-10">
-              <p className="text-lg text-gray-500">Package Delivery Date:</p>
-              <hr className="w-5/6 border-gray-300 lg:my-0 my-5" />
+            <div className='lg:w-2/4 w-full  lg:my-0 my-10'>
+              <p className='text-lg text-gray-500'>Package Delivery Date:</p>
+              <hr className='w-5/6 border-gray-300 lg:my-0 my-5' />
               {DeliveryDate(FormData.State)}
             </div>
           </div>
           {/* </form> */}
 
-          <hr className="w-full mt-5 text-gray-700 border-gray-300" />
-          <h2 className="text-2xl font-bold my-5">Your Contact Information</h2>
+          <hr className='w-full mt-5 text-gray-700 border-gray-300' />
+          <h2 className='text-2xl font-bold my-5'>Your Contact Information</h2>
 
           <input
-            type="checkbox"
+            type='checkbox'
             aria-checked={isChecekd}
             checked={isChecekd}
             onChange={checkHandler}
           />
           {/* <form action="" className="w-full"> */}
-          <div className="mb-4">
-            <label htmlFor="" className="block font-bold">
+          <div className='mb-4'>
+            <label htmlFor='' className='block font-bold'>
               Email Adress
             </label>
             <input
-              type="email"
+              type='email'
               onChange={FormValues}
               value={FormData.email}
-              id="email"
-              className="input-primary"
+              id='email'
+              className='input-primary'
             />
           </div>
-          <div className="mb-8">
-            <label htmlFor="" className="block font-bold">
+          <div className='mb-8'>
+            <label htmlFor='' className='block font-bold'>
               Phone Number
             </label>
             <input
-              type="number"
-              id="number"
+              type='number'
+              id='number'
               onChange={FormValues}
               value={FormData.number}
-              className="input-primary"
+              className='input-primary'
             />
           </div>
           <div>
             <button
-              type="submit"
+              type='submit'
               disabled={!checkForm()}
-              className="disabled:bg-opacity-30 font-bold bg-teal-700 py-3 rounded-md text-white text-[16px] input-primary"
+              className='disabled:bg-opacity-30 font-bold bg-teal-700 py-3 rounded-md text-white text-[16px] input-primary'
             >
               Continue to Payment Information
             </button>
           </div>
         </form>
       </div>
-      <div className="lg:col-span-2 col-span-6  ">
-        <h2 className="font-bold text-2xl">Order Summary</h2>
-        <div className="border border-gray-400 rounded-md px-2 my-5">
-          <h2 className="text-lg font-bold">Shipping</h2>
+      <div className='lg:col-span-2 col-span-6  '>
+        <h2 className='font-bold text-2xl'>Order Summary</h2>
+        <div className='border border-gray-400 rounded-md px-2 my-5'>
+          <h2 className='text-lg font-bold'>Shipping</h2>
 
           <div>
             {Cart.Productss.map((item, index) => {
               if (Cart.getProductQuantity(item.id) > 0) {
                 return (
                   <div
-                    className="flex gap-3 my-2 items-center border-b-2 py-2 "
+                    className='flex gap-3 my-2 items-center border-b-2 py-2 '
                     key={index}
                   >
-                    <div className="  w-1/6  border-gray-300 p-1 rounded-md h-[50px]">
+                    <div className='  w-1/6  border-gray-300 p-1 rounded-md h-[50px]'>
                       <img
                         src={item.image}
                         width={70}
-                        className="w-full h-full"
+                        className='w-full h-full'
                         alt={item.title}
                       />
                     </div>
-                    <p className="text-sm w-3/6">{item.title}</p>
+                    <p className='text-sm w-3/6'>{item.title}</p>
 
-                    <div className="flex-row">
-                      <p className="text-sm"> ₦{item.price}</p>
-                      <p className="font-bold text-sm w-full">
+                    <div className='flex-row'>
+                      <p className='text-sm'> ₦{item.price}</p>
+                      <p className='font-bold text-sm w-full'>
                         Qty {Cart.getProductQuantity(item.id)}
                       </p>
                       <IoTrashBin
-                        role="button"
+                        role='button'
                         onClick={() => Cart.clearCart(item.id)}
-                        className="text-red-600"
-                        type="button"
+                        className='text-red-600'
+                        type='button'
                       />
                     </div>
                   </div>
@@ -289,22 +285,22 @@ const Checkout = () => {
             })}
           </div>
 
-          <p className="">
-            Delivery Fees{" "}
-            <span className="font-bold">
-              {" "}
-              ₦{DeliveryPrice(FormData.State)}{" "}
-            </span>{" "}
+          <p className=''>
+            Delivery Fees{' '}
+            <span className='font-bold'>
+              {' '}
+              ₦{DeliveryPrice(FormData.State)}{' '}
+            </span>{' '}
           </p>
           <p>
-            Tax Fees: <span className="font-bold"> ₦1200</span>
+            Tax Fees: <span className='font-bold'> ₦1200</span>
           </p>
 
-          <hr className="border-2 mt-4 border-gray-300 " />
-          <div className="my-5">
-            <p className="font-sans text-lg">
+          <hr className='border-2 mt-4 border-gray-300 ' />
+          <div className='my-5'>
+            <p className='font-sans text-lg'>
               SubTotal: ₦
-              {(TotalPrice + DeliveryPrice(FormData.State) + TaxFee).toFixed(2)}{" "}
+              {(TotalPrice + DeliveryPrice(FormData.State) + TaxFee).toFixed(2)}{' '}
             </p>
           </div>
         </div>
